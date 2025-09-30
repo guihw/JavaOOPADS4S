914e070e0bd742b8cd48772e39dabd9aca8f5cc5
Guilherme Santos <guilhermess2005g@gmail.com>
2025-09-29 21:53:28 -0300

    Repository criada

---
+++ src/main/java/com/cesuoop/atividadecrud/repository/ProdutoRepository.java
@@ -0,0 +1,6 @@
+package com.cesuoop.atividadecrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository implements JpaRepository<Product, Long> {
+}
